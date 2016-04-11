a392ed94abf18ab19f067696186b4bc93c355a65
David Yuan <davidnyuan@gmail.com>
2016-04-11 11:53:42 -0700

    Finished assignment. Need to run tests

--- SimpleDomainModel/SimpleDomainModel/main.swift
+++ SimpleDomainModel/SimpleDomainModel/main.swift
@@ -27,12 +27,35 @@ public struct Money {
   public var amount : Int
   public var currency : String
   
-  public func convert(to: String) -> Money {
-  }
+  public mutating func convert(to: String) -> Money {
+    let currencyConverter: [String: Double] = ["USD": 1, "GBP": 0.5, "EUR": 1.5, "CAN": 1.25]
+    amount = Int(Double(amount) * (currencyConverter[to]! / currencyConverter[currency]!))
+    currency = to
+    return(Money(amount: self.amount, currency: self.currency))
+    }
   
-  public func add(to: Money) -> Money {
+  public mutating func add(to: Money) -> Money {
+    if(currency == to.currency) {
+        amount += to.amount
+        return(Money(amount: self.amount, currency: self.currency))
+    } else {
+        // Different currencies
+        amount = convert(to.currency).amount
+        amount += to.amount
+        return(Money(amount: self.amount, currency: self.currency))
+    }
   }
-  public func subtract(from: Money) -> Money {
+    
+  public mutating func subtract(from: Money) -> Money {
+    if(currency == from.currency) {
+        amount = amount - from.amount
+        return(Money(amount: self.amount, currency: self.currency))
+    } else {
+        // Different currencies
+        amount = convert(from.currency).amount
+        amount = amount - from.amount
+        return(Money(amount: self.amount, currency: self.currency))
+    }
   }
 }
 
@@ -44,34 +67,56 @@ public class Job {
     case Hourly(Double)
     case Salary(Int)
   }
-  
+    public var title : String
+    public var type : JobType
+    
   public init(title : String, type : JobType) {
+    self.title = title
+    self.type = type
   }
   
   public func calculateIncome(hours: Int) -> Int {
-  }
-  
-  public func raise(amt : Double) {
+    switch type {
+    case .Hourly(let value):
+        return(hours * Int(value))
+    case .Salary(let value):
+        return value
+    }
   }
 }
 
 ////////////////////////////////////
 // Person
 //
+
 public class Person {
   public var firstName : String = ""
   public var lastName : String = ""
   public var age : Int = 0
 
   public var job : Job? {
-    get { }
-    set(value) {
+    get {
+        return self.job
+    }
+    set(inputJob) {
+        if age >= 16 {
+            self.job = Job(title: inputJob!.title, type: inputJob!.type)
+        } else {
+            self.job = nil
+        }
     }
   }
   
   public var spouse : Person? {
-    get { }
+    get {
+        return self.spouse
+    }
     set(value) {
+        if age >= 18 {
+            self.spouse = Person(firstName: value!.firstName, lastName: value!.lastName, age: value!.age)
+        } else {
+            self.spouse = nil
+        }
     }
   }
   
@@ -82,6 +127,7 @@ public class Person {
   }
   
   public func toString() -> String {
+    return("[Person: firstName:\(firstName) lastName:\(lastName) age:\(age) job:\(job) spouse:\(spouse)]")
   }
 }
 
@@ -92,16 +138,38 @@ public class Family {
   private var members : [Person] = []
   
   public init(spouse1: Person, spouse2: Person) {
+    if(spouse1.age >= 18 && spouse2.age >= 18) {
+        if(spouse1.spouse == nil && spouse2.spouse == nil) {
+            spouse1.spouse = spouse2
+            spouse2.spouse = spouse1
+            members.append(spouse1)
+            members.append(spouse2)
+        }
+    }
   }
   
   public func haveChild(child: Person) -> Bool {
+    var canhave = false
+    for person: Person in members {
+        if(person.age >= 21) {
+            canhave = true
+        }
+        if(canhave) {
+            members.append(Person(firstName: child.firstName, lastName: child.lastName, age: child.age))
+        }
+    }
+    return(canhave)
   }
   
   public func householdIncome() -> Int {
+    var totalIncome = 0
+    for person: Person in members {
+        if(person.job != nil) {
+            totalIncome += person.job!.calculateIncome(2000)
+        }
+    }
+    return(totalIncome)
   }
 }
-
-
-
 
 
